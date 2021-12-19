0d9371116a9780d1c2fa4311f5017bf7ed1a1711
stylepatrick <ahrntal.patrick@gmail.com>
2021-12-19 13:55:53 +0100

    initial commit

---
+++ src/main/java/stylepatrick/miningtelegramnotifier/MiningTelegramNotifierApplication.java
@@ -0,0 +1,13 @@
+package stylepatrick.miningtelegramnotifier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiningTelegramNotifierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiningTelegramNotifierApplication.class, args);
+	}
+
+}
---
+++ src/test/java/stylepatrick/miningtelegramnotifier/MiningTelegramNotifierApplicationTests.java
@@ -0,0 +1,13 @@
+package stylepatrick.miningtelegramnotifier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiningTelegramNotifierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
